de32f86ec3ec97deb6896afcaf2ec97111537f83
e1ectron1k <tron1k@yandex.ru>
2025-10-22 21:37:54 +0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,12 @@
-(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.ajs = factory());
-})(this, (function () {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.ajs = factory();
+    }
+}(typeof self !== 'undefined' ? self : this, function () {
     function info() {
         const m = new Map();
         m.set('name', 'ajs');
